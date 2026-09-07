b04b34b06c87186078b4a1064623f422c820ef7b
Andrew <2572947233@qq.com>
2026-09-07 20:39:12 +0800

    开启业务流程编写

--- atguigu/task/command/processor.py
+++ atguigu/task/command/processor.py
@@ -1,4 +1,5 @@
-from atguigu.domain.contexts import CanceledSystemContext
+from atguigu.domain.contexts import CanceledSystemContext, TaskContext, InterruptedSystemContext, ResumedSystemContext, \
+    StartedSystemContext
 from atguigu.domain.state import DialogueState
 from atguigu.task.command.commands import Command, StartFlowCommand, SetSlotsCommand, CancelFlowCommand, \
     ResumeFlowCommand
@@ -67,3 +68,69 @@ class CommandProcessor:
         ))
         # 4. 清空所有流程(业务流程、系统流程都清空)
         state.end_activating_task()
+
+    def _process_start_flow(self, command: StartFlowCommand, state: DialogueState, flow_list: FlowsList):
+        """"
+        开启一个业务流程
+        """
+        # 1. 清空之前的系统流程：(过长白)【不能清空之前业务流程：存起来】
+        state.end_activating_system_task()
+        # 1.1 获取开启业务流程ID
+        start_flow_id = command.flow
+        # 1.2 获取开启业务流程
+        start_flow = flow_list.get_flow_by_id(start_flow_id)
+
+        active_task = state.active_task
+
+        # 2.1 当前有正在运行的业务流程
+        if active_task is not None:
+            # a) 当前正在运行的业务流程刚好是要开启的业务流程
+            if active_task.flow_id == start_flow_id:
+                return
+            # b) 中断当前正在执行的业务流程(当前正在执行的业务流程不等于要开启的业务流程)
+            state.interrupted_activating_task()
+            # c) 从暂停栈中在查询一次  如果这个方法返回false 说明暂存栈中没有回恢复的流程需要新开一个，返回ture说明已经回复就不用新开
+            if not state.resumed_interrupted_business_task(flow_id=start_flow_id):
+                state.start_active_business_task(TaskContext(
+                    flow_id=start_flow_id,
+                    step_id=start_flow.get_start_step().id
+                ))
+            # d) 激活中断的系统流程(不管业务流程要不要激活(创建)，都需要激活中断系统流程。) 注释:应为当前有正在运行的业务流程，所以必须要有中断系统流程参与
+            interrupted_system_flow = flow_list.get_flow_by_id("system_task_interrupted")
+            interrupted_flow_id = active_task.flow_id
+            interrupted_flow_name = flow_list.get_flow_by_id(interrupted_flow_id).flow_name
+            state.start_active_system_task(InterruptedSystemContext(
+                system_flow_id="system_task_interrupted",
+                system_step_id=interrupted_system_flow.get_start_step().id,
+                interrupted_flow_id=interrupted_flow_id,
+                interrupted_flow_name=interrupted_flow_name,
+                started_flow_id=start_flow_id,
+                started_flow_name=start_flow.flow_name
+            ))
+        else:
+            # a) 从暂停栈中在查询一次,如果查询到了当前要开启的业务流程，不用开启业务流程，不用开启开始的系统流程（这个开场白之前已经说过一次）恢复开场白表示出来
+            if state.resumed_interrupted_business_task(flow_id=start_flow_id):
+                resumed_system_flow = flow_list.get_flow_by_id("system_task_resumed")
+                state.start_active_system_task(ResumedSystemContext(
+                    system_flow_id=resumed_system_flow.flow_id,
+                    system_step_id=resumed_system_flow.get_start_step().id,
+                    resumed_flow_id=start_flow_id,
+                    resumed_flow_name=flow_list.get_flow_by_id(state.active_task.flow_id).flow_name
+                ))
+                return
+            # b) 从暂停栈中未查询到当前要开启的业务流程
+            # 1. 激活当前业务流程
+            state.start_active_business_task(TaskContext(
+                flow_id=start_flow_id,
+                step_id=start_flow.get_start_step().id
+            ))
+            # 2. 激活开始系统流程
+            start_system_flow = flow_list.get_flow_by_id("system_task_started")
+            state.start_active_system_task(StartedSystemContext(
+                system_flow_id="system_task_started",
+                system_step_id=start_system_flow.flow_id,
+                started_flow_id=start_flow_id,
+                started_flow_name=start_flow.flow_name
+            ))
+
+        pass
